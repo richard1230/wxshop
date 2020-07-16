773bbc5db28a0d7412ad6b640dbbed454d26228e
richard1230 <yongjinhu1230@gmail.com>
2020-07-16 10:05:53 +0800

    finish creating order module and order interfer

--- wxshop-main/src/test/java/com/github/wxshop/WxshopApplicationTests.java
+++ wxshop-main/src/test/java/com/github/wxshop/WxshopApplicationTests.java
@@ -1,12 +1,10 @@
 package com.github.wxshop;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WxshopApplicationTests {
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
